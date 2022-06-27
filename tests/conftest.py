d84e09ef9f068e30555674d0aac4335386ce627a
Ricardo Cisterna <r.cisternasantos@gmail.com>
2022-06-27 18:42:33 -0400

    test: add first version of the test suite

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+"""
+Configuration for general tests.
+"""
+import pytest
+from pytest_mock import MockerFixture
+
+
+@pytest.fixture(scope="class")
+def mock_cc_get_commit_msg(request: pytest.FixtureRequest, class_mocker: MockerFixture):
+    """Fixture to patch clint.validator.get_commit_msg function."""
+    request.cls.mock_cc_get_commit_msg = class_mocker.patch(
+        "clint.validator.get_commit_msg"
+    )
+
+
+@pytest.fixture(scope="class")
+def mock_sys_exit(request: pytest.FixtureRequest, class_mocker: MockerFixture):
+    """Fixture to patch sys.exit function."""
+    request.cls.mock_sys_exit = class_mocker.patch("sys.exit")
