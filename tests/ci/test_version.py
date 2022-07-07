e494f4c539a2409b52f8a0cf8c2483de24f8e66a
Ricardo Cisterna <r.cisternasantos@gmail.com>
2022-07-07 13:47:47 -0400

    refactor: add package for ci tests

---
+++ tests/ci/test_version.py
@@ -0,0 +1,30 @@
+"""Test suite for special validations in CI."""
+from unittest.mock import MagicMock
+
+import pytest
+
+
+@pytest.mark.ci_version
+@pytest.mark.usefixtures("metadata_current", "metadata_production")
+class TestVersion:
+    """Test for version."""
+
+    metadata_current: MagicMock
+    metadata_production: MagicMock
+
+    def test_is_different(self):
+        """Verify current clint version is distinct to production version."""
+        version_current: str = self.metadata_current["tool"]["poetry"]["version"]
+        version_production: str = self.metadata_production["tool"]["poetry"]["version"]
+        assert version_current != version_production
+
+    def test_current_is_higher(self):
+        """Verify current clint version is higher than production version."""
+        version_current: str = self.metadata_current["tool"]["poetry"]["version"]
+        version_production: str = self.metadata_production["tool"]["poetry"]["version"]
+        curr_versions = tuple(int(v) for v in version_current.split("."))
+        main_versions = tuple(int(v) for v in version_production.split("."))
+        for curr_version, main_version in zip(curr_versions, main_versions):
+            assert curr_version >= main_version
+            if curr_version > main_version:
+                break
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,8 @@
 """Configuration for general tests."""
-import os.path
-
 import pytest
-import toml
-import urllib3
 from faker import Faker
+
+from .ci.conftest import metadata_current  # pylint: disable=unused-import
 
 faker = Faker()
 
@@ -27,22 +25,3 @@ def mock_sys_exit(request, class_mocker):
 def sentence() -> str:
     """Fixture to get a faker sentence."""
     return faker.sentence()
-
-
-@pytest.fixture
-def clint_metadata(pytestconfig) -> dict:
-    """Fixture to get the CLint metadata as dictionary."""
-    pyproject_path = os.path.join(pytestconfig.rootpath, "pyproject.toml")
-    with open(pyproject_path, mode="r", encoding="utf8") as pyproject:
-        metadata = toml.load(pyproject)
-    return metadata
-
-
-@pytest.fixture
-def clint_main_metadata() -> dict:
-    """Fixture to get the CLint metadata as dictionary."""
-    url = "https://raw.githubusercontent.com/rcisterna/clint/main/pyproject.toml"
-    http = urllib3.PoolManager()
-    with http.request("GET", url, preload_content=False) as response:
-        pyproject_str = response.data.decode("utf8")
-    return toml.loads(pyproject_str)
